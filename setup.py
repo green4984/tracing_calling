1b571bd295520a7c64685ee4a23a2b40abbdc032
liuyang5-s <liuyang5-s@360.cn>
2016-08-08 19:46:46 +0800

    to make setup.py sdist

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(
+        name = 'tracing_calling',
+        version = 0.1,
+        packages = find_packages(),
+        author = 'green',
+        author_email = 'green.4984@gmail.com',
+        url = 'git@github.com:green4984/tracing_calling.git',
+        license = 'http://www.apache.org/licenses/LICENSE-2.0.html',
+        description = 'tracking the calling method'
+)
